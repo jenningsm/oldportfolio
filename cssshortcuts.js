7ef4ac76416b35ef44f9fa4cabf9be492eb8061b
Michael Jennings <mpjngs@gmail.com>
2015-04-03 20:31:55 -0700

    random changes

---
+++ cssshortcuts.js
@@ -0,0 +1,16 @@
+
+function linearGradient(dir, start, stop){
+  return prefix('background', 'linear-gradient(to ' + dir + ', ' + start + ', ' + stop + ')');
+}
+
+function prefix(style, value, prefixes){
+  if(prefixes === undefined){
+    prefixes = ['webkit', 'o', 'moz'];
+  }
+  var prefixed = {};
+  for(var i = 0; i < prefixes.length; i++){
+    prefixed['-' + prefixes[i] + '-' + style] = value;
+  }
+  prefixed[style] = value;
+  return prefixed;
+}
