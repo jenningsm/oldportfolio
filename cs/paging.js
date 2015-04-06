2f876e3de9b562eceef60fc43bdf21fe81879465
Michael Jennings <mpjngs@gmail.com>
2015-04-06 00:35:58 -0700

    transition opacity change again

--- cs/paging.js
+++ cs/paging.js
@@ -18,12 +18,12 @@ for (var i = 0; i < keys.length; i++){
 function toPage(from, to, dir){
   var move = motion(3, 3);
 
-  function childOpacity(element, opacity){
+/*  function childOpacity(element, opacity){
     var c = element.children;
     for(var i = 0; i < c.length; i++){
       c[i].style.opacity = opacity;
     }
-  }
+  }*/
 
   var speed = .02;
   var time = 0;
@@ -37,23 +37,23 @@ function toPage(from, to, dir){
       from.style.transform = "translate3d(0, " + shift + '%, 0)';
       to.style.transform = "translate3d(0, " + (toStart + shift) + '%, 0)';
       var pro = .6;
-      //from.style.opacity = Math.max(0, (1 - time / pro))
-      //to.style.opacity = Math.max(0, (time - 1 + pro) / pro);
-      childOpacity(from, Math.max(0, (1 - time / pro)));
-      childOpacity(to, Math.max(0, (time - 1 + pro) / pro));
+      from.style.opacity = Math.max(0, (1 - time / pro))
+      to.style.opacity = Math.max(0, (time - 1 + pro) / pro);
+      //childOpacity(from, Math.max(0, (1 - time / pro)));
+      //childOpacity(to, Math.max(0, (time - 1 + pro) / pro));
       requestAnimationFrame(transition);
     } else {
       from.style.transform = "translate3d(0, " + (dir ? -100 : 100) + '%, 0)';
       from.style.display = "none";
       to.style.transform = "translate3d(0, 0, 0)";
-      //to.style.opacity = 1;
-      childOpacity(to, 1);
+      to.style.opacity = 1;
+      //childOpacity(to, 1);
     }
   }
 
   to.style.transform = "translate3d(0, " + toStart + '%, 0)';
   to.style.display = 'block';
-  //to.style.opacity = 0;
-  childOpacity(to, 0);
+  to.style.opacity = 0;
+  //childOpacity(to, 0);
   requestAnimationFrame(transition);
 }
