6ffcf326029952f40dfe1b3dc71bf1c2fb47b500
Michael Jennings <mpjngs@gmail.com>
2015-04-23 15:23:03 -0700

    started projects

--- pages/pages.js
+++ pages/pages.js
@@ -1,8 +1,7 @@
-var coms = require('./components.js')
 var Element = require('/home/mjennings/pagebuilder/html.js')
+var coms = require('./components.js')
 var connectPages = require('./connect.js')
 var colors = require('../colors.js')
-var css = require('../css.js')
 var box = require('./box.js')
 var exp = require('./experience.js')
 var temp = require('./templates.js')
@@ -13,7 +12,10 @@ var temp = require('./templates.js')
 module.exports = function(){
   var structure = [
     title(),
-      dummy('projects'),
+      [projects(),
+        dummy('deftly'),
+        dummy('electrodynamics'),
+        dummy('charlibeck')],
       about(),
       contact(),
       [exp.experience(),
@@ -28,6 +30,24 @@ module.exports = function(){
 
 /////////////////////////////////////////////////
 
+function projects(){
+  var content = new Element('div').content(
+    new Element('p').content(
+      "Here's some things I've been working on lately:"
+    ),
+    new Element('p').content(
+      coms.link("Deftly", 'deftly', 'up', 'push'),
+      ", a website I built to display some of my generative sketches.",
+      "<br/>",
+      coms.link("Electrodynamics", 'electrodynamics', 'up', 'push'),
+      ", an Android app that creates lightning bolt visuals.",
+      "<br/>",
+      coms.link("A porfolio website", 'charlibeck', 'up', 'push'),
+      " for an illustrator, Charli Beck."
+    )
+  )
+  return temp.linkedContainer('projects', content, false, true, 50)
+}
 
 function education(){
   var div = temp.titledInfo(
@@ -55,7 +75,6 @@ function contact(){
 }
 
 
-
 function about(){
   var content = new Element('div').content(
     'My name is Michael Jennings. This is my site. You ' +
@@ -64,6 +83,7 @@ function about(){
 
   return temp.linkedContainer('about', content)
 }
+
 
 function title(){
   return {
